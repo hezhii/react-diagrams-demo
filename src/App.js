5965dd08ba01ae9b70efb361bccb46290b36840c
biu <hezhou920@gmail.com>
2018-08-10 13:57:16 +0800

    simple demo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import 'storm-react-diagrams/dist/style.min.css';
+
 import './App.css';
+import Home from './routes/Home';
+import Simple from './routes/Simple';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/simple" component={Simple} />
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/routes/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default () => {
+  return (
+    <div>
+      <h1>Demos of React Diagrams</h1>
+      <ul>
+        <li>
+          <Link to="simple">Simple</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}

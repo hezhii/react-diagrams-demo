0de4d277dbd12762b1df822ee3a181a213e15c9d
biu <hezhou920@gmail.com>
2018-08-13 13:49:20 +0800

    Update title; Use hash router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {HashRouter as Router, Switch, Route} from 'react-router-dom';
 import 'storm-react-diagrams/dist/style.min.css';
 
 import './App.css';
